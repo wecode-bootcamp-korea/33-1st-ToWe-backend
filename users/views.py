8694902a3b7d40bdd6b4284c127aee42f12b86ce
Brian-with <chiyoungv@gmail.com>
2022-05-26 17:42:31 +0900

    로그인 1.0

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
-import json, bcrypt
+import json, bcrypt, jwt
+from datetime import datetime, timedelta
 
 from django.views import View
 from django.http import JsonResponse
 from django.core.exceptions import ValidationError
+from django.conf import settings
 
 from .models import User
 from .validator import validate_email, validate_password
@@ -35,6 +37,31 @@ class SignupView(View):
                 )
 
             return JsonResponse({"MESSAGE": "SUCCESS"}, status=201)
+
+        except KeyError :
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
+
+        except ValidationError as verr :
+            return JsonResponse({"MESSAGE": verr.message}, status=400)
+
+class LoginView(View):
+    def post(self, request):
+        
+        try :
+            login_data = json.loads(request.body)
+            email      = login_data["email"]
+            password   = login_data["password"]
+
+            if not User.objects.filter(email=email).exists() :
+                raise ValidationError("INVALID_USER")
+
+            user = User.objects.get(email=email)
+            if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')) :
+                raise ValidationError("INCORRECT_PASSWORD")
+
+            token = jwt.encode( {'id': user.id, 'exp': datetime.utcnow()+timedelta(days=3)}, settings.SECRET_KEY, settings.ALGORITHM)
+
+            return JsonResponse({"TOKEN": token}, status=201)
 
         except KeyError :
             return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)

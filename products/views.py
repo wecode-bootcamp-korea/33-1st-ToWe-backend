4da0d0e0920ee443ede63da6354d25f66e2b5931
byeonghwijeong <byeonghwijeong@gmail.com>
2022-05-26 18:16:26 +0900

    제품상세 1.2

--- products/views.py
+++ products/views.py
@@ -5,24 +5,22 @@ from products.models import Product
 
 
 class ProductDetailView(View):
-	def get(self, request, product_id):
-		try:
-			product = Product.objects.get(pk = product_id)
-			images  = [image.url for image in product.imageurl_set.all()]
-			colors  = [productoption.color.color for productoption in product.productoption_set.all()]
-
-			product_detail = [
-				{
-					'product_id' : product.id,
-					'name' : product.name,
-					'price' : product.price,
-					'description' : product.description,
-					'image_url' : images,
-					'color' :colors,
-
-				}
-			]
-			return JsonResponse({'product_detail': product_detail}, status =200)
-		except Product.DoesNotExist:
-			return JsonResponse({'message': 'DOES_NOT_EXIST'}, status = 400)
+    def get(self, request, product_id):
+        try:
+            product = Product.objects.get(pk = product_id)
+            images  = [image.url for image in product.imageurl_set.all()]
+            colors  = [productoption.color.color for productoption in product.productoption_set.all()]
+            product_detail = [
+                {
+                    'product_id' : product.id,
+                    'name' : product.name,
+                    'price' : product.price,
+                    'description' : product.description,
+                    'image_url' : images,
+                    'color' :colors,
+                }
+            ]
+            return JsonResponse({'product_detail': product_detail}, status =200)
+        except Product.DoesNotExist:
+            return JsonResponse({'message': 'DOES_NOT_EXIST'}, status = 400)
 

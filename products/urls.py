69a5845677974699ff4768a0850c86138229d21a
byeonghwijeong <byeonghwijeong@gmail.com>
2022-05-26 17:08:04 +0900

    제품상세 1.1

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from products.views import ProductDetailView
 
 urlpatterns = [
-  path('/<int:product_id>', ProductDetailView.as_view())    
+	path('/<int:product_id>', ProductDetailView.as_view())    
 ]

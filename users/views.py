37d236e5efe206a3969da32b610636b7de163466
Brian-with <chiyoungv@gmail.com>
2022-05-26 13:55:15 +0900

    회원가입 1.0

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
+
+from .models import User
+
+class SignupView(View):
+    def post(self, request):
+        
+        try:
+            data = json.loads(request.body)
+
+            User.objects.create(
+                name =
+                email =
+                password =
+                phone_number =
+                address =
+            )
+
+

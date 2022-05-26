339802b9cfe9a134af32ec191d451e150fe954d1
byeonghwijeong <byeonghwijeong@gmail.com>
2022-05-26 16:15:32 +0900

    제품상세 1.0

--- products/views.py
+++ products/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
 
-# Create your views here.
+from products.models import Product
+
+
+class ProductDetailView(View):
+	def get(self, request, product_id):
+		try:
+			product = Product.objects.get(pk = product_id)
+			images  = [image.url for image in product.imageurl_set.all()]
+			colors  = [productoption.color.color for productoption in product.productoption_set.all()]
+
+			product_detail = [
+				{
+					'product_id' : product.id,
+					'name' : product.name,
+					'price' : product.price,
+					'description' : product.description,
+					'image_url' : images,
+					'color' :colors,
+
+				}
+			]
+			return JsonResponse({'product_detail': product_detail}, status =200)
+		except Product.DoesNotExist:
+			return JsonResponse({'message': 'DOES_NOT_EXIST'}, status = 400)
+

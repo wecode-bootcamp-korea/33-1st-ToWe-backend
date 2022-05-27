3a93494aa625a6064beaa86ab8a6395c05540ef7
Brian-with <chiyoungv@gmail.com>
2022-05-27 13:20:19 +0900

    장바구니 1.0

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+
+from .models import Cart
+from products.models import ProductOption
+from towe.utils import login_decorator
+
+class CartView(View):
+    @login_decorator
+    def post(self, request):
+
+        data = json.loads(request.body)
+
+        product_id = data["product_id"]
+        quantity = data["quantity"]
+
+        Pro
+        user = request.user
+
+
+        Cart.objects.create(
+            user_id = user.id
+
+        )

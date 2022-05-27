d20515c52aa578b0f5f8280efada06a79f974a05
Brian-with <chiyoungv@gmail.com>
2022-05-27 10:48:07 +0900

    마이페이지 1.3

--- towe/utils.py
+++ towe/utils.py
@@ -8,8 +8,8 @@ from users.models import User
 def login_decorator(func):
     def wrapper(self, request, *args, **kargs):
         try:
-            token = request.headers.get('Authorization')
-            payload = jwt.decode(token, settings.SECRET_KEY, settings.ALGORITHM)
+            token        = request.headers.get('Authorization')
+            payload      = jwt.decode(token, settings.SECRET_KEY, settings.ALGORITHM)
             request.user = User.objects.get(id=payload['id'])
 
             return func(self, request, *args, **kargs)

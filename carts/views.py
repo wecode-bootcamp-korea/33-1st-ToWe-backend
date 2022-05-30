bb4d711ce1249b3e9a41d2d30e9194c48d32b10a
Brian-with <chiyoungv@gmail.com>
2022-05-30 13:32:37 +0900

    장바구니 보기 1.0

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Cart
+from towe.utils import login_decorator
+
+class CartView(View):
+    @login_decorator
+    def get(self, request):
+        
+        result = []
+        for cart in Cart.objects.filter(user_id=request.user.id):
+            product = cart.product
+            result.append({
+                'id':cart.id,
+                'product_name':cart.product.product.name,
+                'color':cart.product.color.color,
+                'price':cart.product.product.price,
+                'thumbnail_url':cart.product.product.thumbnail_img_url
+            })
+        
+        return JsonResponse({'result':result}, status=200)

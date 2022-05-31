f4bdac48256ef74259a0006e7a8d78f4d3e229a6
byeonghwijeong <byeonghwijeong@gmail.com>
2022-05-31 14:30:16 +0900

    좋아요 1.1

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import SignupView, LoginView, UserDetailView
+from .views import SignupView, LoginView, UserDetailView, LikeView
 
 urlpatterns = [
     path("/signup", SignupView.as_view()),
     path("/login", LoginView.as_view()),
     path("/detail", UserDetailView.as_view()),
+    path("/like", LikeView.as_view())
 ]

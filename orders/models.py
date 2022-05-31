14dafe39b7eecead9e83f8f91f5cfe899419e4ab
Brian-with <chiyoungv@gmail.com>
2022-05-31 18:30:41 +0900

    주문관리 모델작성 1.2

--- orders/models.py
+++ orders/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 from core.models import TimeStampModel
 
-class OrderStates(models.Model):
-    state = models.CharField(max_length=45)
+class OrderDetailStatus(models.Model):
+    name = models.CharField(max_length=45)
 
     class Meta:
-        db_table = 'order_states'
+        db_table = 'order_detail_statuses'
 
 class Order(TimeStampModel):
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
@@ -15,10 +15,10 @@ class Order(TimeStampModel):
         db_table = 'orders'
 
 class OrderDetail(TimeStampModel):
-    order          = models.ForeignKey("orders.Order", on_delete=models.CASCADE)
-    product_option = models.ForeignKey("products.ProductOption", on_delete=models.CASCADE)
-    order_state    = models.ForeignKey("orders.OrderStates", on_delete=models.CASCADE)
-    quantity       = models.IntegerField()
+    order               = models.ForeignKey("orders.Order", on_delete=models.CASCADE)
+    product_option      = models.ForeignKey("products.ProductOption", on_delete=models.CASCADE)
+    order_detail_status = models.ForeignKey("orders.OrderDetailStatus", on_delete=models.CASCADE)
+    quantity            = models.IntegerField()
     
     class Meta:
         db_table = 'order_details'

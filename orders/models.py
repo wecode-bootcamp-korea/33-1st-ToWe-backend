d4c2ef8a3990ac5610ef4fb6c19cf56f94c7ed09
Brian-with <chiyoungv@gmail.com>
2022-05-31 16:42:08 +0900

    주문관리 모델작성 1.0

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampModel
+
+class OrderStates(models.Model):
+    state = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'order_states'
+
+class Order(TimeStampModel):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'orders'
+
+class OrderDetail(TimeStampModel):
+    order          = models.ForeignKey("orders.Order", on_delete=models.CASCADE)
+    product_option = models.ForeignKey("products.ProductOption", on_delete=models.CASCADE)
+    order_state    = models.ForeignKey("orders.OrderStates", on_delete=models.CASCADE)
+    quantity       = models.IntegerField()
+    
+    class Meta:
+        db_table = 'order_details'
